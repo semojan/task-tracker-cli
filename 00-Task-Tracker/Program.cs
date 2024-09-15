baee0f99b0b9ccf03176c2f676e0db0ed759d0a6
semojan <97410424+semojan@users.noreply.github.com>
2024-09-15 11:42:21 +0330

    Add project files.

---
+++ 00-Task-Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_Task_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
