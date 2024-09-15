429e48ddd737097979e3352f8ae5fdd53b0171d1
semojan <97410424+semojan@users.noreply.github.com>
2024-09-15 12:03:12 +0330

    added tasks class

---
+++ 00-Task-Tracker/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Task_Tracker
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+        public DateTime createdAt { get; set; } 
+        public DateTime updatedAt { get; set; }
+
+    }
+}
